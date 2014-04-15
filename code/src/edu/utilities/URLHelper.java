3b301a0cb6b3147340f77b5c68e99376d59f4b53
Sri Kiran Kanchibhotla <kvrs.kiran@gmail.com>
2014-04-15 02:57:54 -0400

    Created a URL utility
    To be used by HBase Mapper and Reducer classes for creating row-keys

---
+++ code/src/edu/utilities/URLHelper.java
@@ -0,0 +1,112 @@
+package edu.utilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class URLHelper {
+
+	/**
+	 * @param args
+	 * @throws NoSuchAlgorithmException
+	 */
+	private URL url;
+
+	public URL getUrl() {
+		return url;
+	}
+
+	/*
+	 * Sets URL using a given string. If not a valid URL, set to null
+	 */
+	public void setURL(String url) {
+		try {
+			this.url = new URL(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			this.url = null;
+		}
+	}
+
+	public URLHelper(String url) {
+		try {
+			this.url = new URL(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			this.url = null;
+		}
+	}
+
+	public URLHelper() {
+	}
+
+	/*
+	 * Generates SHA from the URL object
+	 */
+	protected String sha1() throws NoSuchAlgorithmException {
+		MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+		byte[] result = mDigest.digest(url.toString().getBytes());
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < result.length; i++) {
+			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16)
+					.substring(1));
+		}
+		return sb.toString();
+	}
+
+	/*
+	 * The substring of SHA is used to form HBase row key
+	 */
+	private String subSha1() throws NoSuchAlgorithmException {
+		return sha1().substring(0, 10);
+	}
+
+	private String getHost() {
+		return url.getHost();
+	}
+
+	/*
+	 * public String generateKey() throws NoSuchAlgorithmException {
+	 * 
+	 * }
+	 */
+
+	public String getReversedHost() {
+		StringBuilder rHost = new StringBuilder();
+		String[] parts = getHost().split("\\.");
+		for (int i = parts.length - 1; i > 0; i--) {
+			rHost.append(parts[i]).append(".");
+		}
+		rHost.append(parts[0]);
+		return rHost.toString();
+	}
+
+	private StringBuilder getRHost() {
+		StringBuilder rHost = new StringBuilder();
+		String[] parts = getHost().split("\\.");
+		for (int i = parts.length - 1; i > 0; i--) {
+			rHost.append(parts[i]).append(".");
+		}
+		rHost.append(parts[0]);
+		return rHost;
+	}
+
+	public String getKey() throws NoSuchAlgorithmException {
+		StringBuilder key = getRHost();
+		key.append("-").append(subSha1());
+		return key.toString();
+	}
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		URLHelper uh = new URLHelper();
+		uh.setURL("http://www.sha1-online.com/sha1-java/");
+		System.out.println(uh.sha1());
+		uh.setURL("http://www.sha1-online.com/");
+		System.out.println(uh.sha1());
+		System.out.println(uh.subSha1());
+		System.out.println(uh.getHost());
+		System.out.println(uh.getReversedHost());
+		System.out.println(uh.getKey());
+	}
+}
